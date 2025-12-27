3b43f7466ebc69d61d276d63b66aacd054abd0f7
Daniel Pitts <40121003+dpitts00@users.noreply.github.com>
2025-12-27 15:38:03 -0600

    Initial Commit

---
+++ HomeListDemo/HomeListDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeListDemoApp.swift
+//  HomeListDemo
+//
+//  Created by Daniel Pitts on 12/27/25.
+//
+
+import SwiftUI
+
+@main
+struct HomeListDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HomeListDemoTests/HomeListDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  HomeListDemoTests.swift
+//  HomeListDemoTests
+//
+//  Created by Daniel Pitts on 12/27/25.
+//
+
+import Testing
+@testable import HomeListDemo
+
+struct HomeListDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
